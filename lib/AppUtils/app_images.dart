ffe15732d501cf37871ea99649b450357737c5f1
khuzaifa17 <Khuzaifaazam17@gmail.com>
2024-07-28 21:06:21 +0500

    first commit

---
+++ lib/AppUtils/app_images.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter_expense_manager/AppUtils/app_constants.dart';
+
+class AppImages {
+  // Base ImagePath
+
+  static const String baseImagePath = AppConstants.baseImagePath;
+  static const String appIconImg = '$baseImagePath/appicon.png';
+}
